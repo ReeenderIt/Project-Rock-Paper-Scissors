53f59737d1c6f46fb9ec95e4821f01606073a304
Ren <developer.ren28@gmail.com>
2023-01-15 03:00:26 +0800

    Add a switch statement to assign the numbers to Rock, Paper, Scissor, or run the whole function code again

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 10);
 
     console.log(randomNumber);
+
+    switch (randomNumber) {
+        case 0:
+        case 1:
+        case 2:
+            console.log("Rock");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Paper");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Scissors");
+            break;
+        default:
+            getComputerChoice();
+    };
 };
 
 getComputerChoice();
