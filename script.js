4d3d23764a6b48033187f65aadf5e9be8487e711
Ren <developer.ren28@gmail.com>
2023-01-26 03:14:55 +0800

    Delete unnecessary lines

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-
-let x;
-
-// Generate random number from 0 - 9
-// Divide the numbers to sets: [0, 1, 2], [3, 4, 5], [6, 5, 7]
-// Assign sets to the outcome:
-// [0, 1, 2] - Rock
-// [3, 4, 5] - Paper
-// [6, 7, 8] - Scissors
-// The number generated will select the outcome
-// If number generated is 9, run the code again
-
 function getComputerChoice() {
-    
+
     let randomNumber = Math.floor(Math.random() * 10);
 
     let choice;
@@ -39,20 +27,6 @@ function getComputerChoice() {
     return choice;
 };
 
-
-// Game's Rules
-// Rock beats Scissors
-// Scissors beats Paper
-// Paper beats Rock
-// If player chooses Rock and computer chooses Scissors or 
-// player chooses Scissors and computer chooses Paper or
-// player chooses Paper and computer chooses Rock,
-// the player wins
-// If player chooses Rock and computer chooses Paper or 
-// player chooses Scissors and computer chooses Rock or
-// player chooses Paper and computer chooses Scissors,
-// the player lose
-// If they choose the same, it's a tie
 
 function playRound(playerSelection, computerSelection) {
 
@@ -82,6 +56,7 @@ function playRound(playerSelection, computerSelection) {
 
     return result;
 };
+
 
 function game() {
 
@@ -114,6 +89,7 @@ function game() {
         console.log(`Computer is the ultimate winner!`);
     };
 };
+
 
 function verify(playerInput) {
 
